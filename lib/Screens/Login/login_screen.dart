5d4b54645c6e09f35aba8c02f028cab547757d8e
Zubaer Alam Bihim <zubaerbihim@gmail.com>
2020-11-02 00:44:26 +0600

    Update login_screen.dart
    Added Login

--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -1,2 +1,8 @@
 import 'package:flutter/material.dart';
 
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(); //https://youtu.be/ExKYjqgswJg?t=423
+  }
+}
