6b35f577f17022f7def0f4b7e1cd5ce728106d22
Zubaer Alam Bihim <zubaerbihim@gmail.com>
2020-11-03 07:23:01 +0600

    Added some classes and bla bla bla

---
+++ lib/Screens/Login/components/body.dart
@@ -0,0 +1,54 @@
+import 'package:first_app/Screens/Login/components/background.dart';
+import 'package:first_app/components/already_have_an_account_check.dart';
+import 'package:first_app/components/rounded_button.dart';
+import 'package:first_app/components/rounded_input_field.dart';
+import 'package:first_app/components/rounded_password_field.dart';
+import 'package:first_app/components/text_field_container.dart';
+import 'package:first_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Login",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: size.height * 0.03,),
+          SvgPicture.asset(
+            "assets/icons/login.svg",
+            height: size.height * 0.3,
+          ),
+          SizedBox(height: size.height * 0.03,),
+          RoundedInputField(
+            hintText: "Your Email",
+            onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          SizedBox(height: size.height * 0.03,),
+          RoundedButton(
+            text: "LOGIN",
+            press: () {},
+          ),
+          SizedBox(height: size.height * 0.03,),
+          AlreadyHaveAnAccountCheck(
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/rounded_input_field.dart
@@ -0,0 +1,31 @@
+import 'package:first_app/components/text_field_container.dart';
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class RoundedInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key key, this.hintText, this.icon = Icons.person, this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
